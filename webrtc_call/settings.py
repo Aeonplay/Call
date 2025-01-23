22069b6ab5068acb29a3da2df63f48bb0bcd2f37
Aeonplay <82922118+Aeonplay@users.noreply.github.com>
2025-01-23 20:19:26 +0000

    Whitenoise added to server static files

--- webrtc_call/settings.py
+++ webrtc_call/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+p!z8s=%(l-m6ywx@m%!fkt(p4#g@+l#8onoj%8+^*6n43lx*&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'overseas-mariya-vyrazu-2b919bb0.koyeb.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
